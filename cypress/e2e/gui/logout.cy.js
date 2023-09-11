472c272332ed1051620ac1444e75af01d867a5fa
Jefferson Borges <jeffersondasilvaborges92@gmail.com>
2023-09-11 11:31:34 -0300

    added session validation tests with cy.session

--- cypress/e2e/gui/logout.cy.js
+++ cypress/e2e/gui/logout.cy.js
@@ -1,9 +1,14 @@
 /// <reference types="Cypress" />
 
 describe('Logout', () => {
+    beforeEach(() => {
+        cy.login()
+        cy.visit('/')
+    })
+
     it('sucessfully', () => {
       cy.logout()
 
-      cy.get("[data-qa-selector='sign_in_tab']").should('be.visible')
+      cy.url().should('be.equal', `${Cypress.config('baseUrl')}/users/sign_in`)
     })
   })
--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,6 +1,7 @@
 Cypress.Commands.add('login', (
     user = Cypress.env('user_name'),
     password = Cypress.env('user_password'),
+    { cacheSession = true } = {},
 ) => {
     const login = () => {
         cy.visit('/users/sign_in')
@@ -10,17 +11,34 @@ Cypress.Commands.add('login', (
         cy.get("[data-qa-selector='sign_in_button']").click()
     }
 
-    login()
+    const validate = () => {
+        cy.visit('/')
+        cy.location('pathname', { timeout: 1000 })
+            .should('not.eql', '/users/sign_in')
+    }
+
+    const options = {
+        cacheAcrossSpecs: true,
+        validate,
+    }
+
+    if (cacheSession) {
+        cy.session(user, login, options)
+    } else {
+        login()
+    }
 })
 
 Cypress.Commands.add('logout', () => {
-    cy.login()
+    cy.get('.qa-user-avatar').click()
+    cy.contains('Sign out').click()
+})
 
-    const logout = () => {
+Cypress.Commands.add('gui_createProject', project => {
+    cy.visit('/projects/new')
 
-        cy.get("[data-qa-selector='user_menu']").click()
-        cy.get("[data-qa-selector='sign_out_link']").click()
-    }
-
-    logout()
+    cy.get('#project_name').type(project.name)
+    cy.get('#project_description').type(project.description)
+    cy.get('.qa-initialize-with-readme-checkbox').check()
+    cy.contains('Create project').click()
 })
